76f5a61d81effc9a518cd4f8b3f38971f615a050
dmitryvim <dmitry.mikhailovich@gmail.com>
2017-11-26 15:16:10 +0300

    add file handler

---
+++ src/main/java/com/mikhaylovich/cache/FileHandler.java
@@ -0,0 +1,60 @@
+package com.mikhaylovich.cache;
+
+import java.io.*;
+import java.util.Optional;
+
+/**
+ * Created by dmitry on 26.11.17. ${PATH}
+ */
+public class FileHandler<T> {
+    private final File file;
+
+    private final Class<T> clazz;
+
+    public FileHandler(File file, Class<T> clazz) {
+        this.file = file;
+        this.clazz = clazz;
+    }
+
+    //TODO add file lock
+    public Optional<T> read() {
+        try (FileInputStream fileInputStream = new FileInputStream(this.file)) {
+            return read(fileInputStream);
+        } catch (FileNotFoundException e) {
+            return Optional.empty();
+        } catch (IOException e) {
+            throw illegalStateException();
+        }
+    }
+
+    public void write(T object) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(this.file)) {
+            write(object, fileOutputStream);
+        } catch (IOException e) {
+            throw illegalStateException();
+        }
+    }
+
+    private Optional<T> read(InputStream inputStream) throws IOException {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+            Object object = objectInputStream.readObject();
+            if (clazz.isInstance(object)) {
+                return Optional.of(object).map(this.clazz::cast);
+            } else {
+                throw illegalStateException();
+            }
+        } catch (ClassNotFoundException e) {
+            throw illegalStateException();
+        }
+    }
+
+    private void write(T object, OutputStream outputStream) throws IOException {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)) {
+            objectOutputStream.writeObject(object);
+        }
+    }
+
+    private IllegalStateException illegalStateException() {
+        return new IllegalStateException("Unable to read object class " + this.clazz + " from file " + this.file);
+    }
+}
---
+++ src/test/java/com/mikhaylovich/cache/FileHandlerTests.java
@@ -0,0 +1,43 @@
+package com.mikhaylovich.cache;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dmitry on 26.11.17. ${PATH}
+ */
+public class FileHandlerTests {
+
+    @Test
+    public void shouldReadAndWriteSameString() throws Exception {
+        // given
+        File file = File.createTempFile("cache_", "_test");
+        FileHandler<String> handler = new FileHandler<>(file, String.class);
+        String string = "String";
+
+        // when
+        handler.write(string);
+        Optional<String> read = handler.read();
+
+        // then
+        assertTrue(read.isPresent());
+        assertEquals(read.get(), string);
+    }
+
+    @Test
+    public void shouldReturnEmptyObjectFromEmptyFile() throws Exception {
+        // given
+        File file = new File("some_unpresent_name");
+        FileHandler<String> handler = new FileHandler<>(file, String.class);
+
+        // when
+        Optional<String> read = handler.read();
+
+        // then
+        assertFalse(read.isPresent());
+    }
+}
